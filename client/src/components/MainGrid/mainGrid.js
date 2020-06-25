38eacaf93e0921d2296161b69a55821c98a62a09
Emily M <30880308+e0marina@users.noreply.github.com>
2020-06-24 19:59:39 -0700

    modal works on image click

--- client/src/components/MainGrid/mainGrid.js
+++ client/src/components/MainGrid/mainGrid.js
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import StackGrid from "react-stack-grid";
 import PaintingCard from "../PaintingCard/index.js";
-
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import "./mainGrid.css";
 
 const MainGrid = ({ paintings }) => {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
   return (
     <div id="container">
       <StackGrid columnWidth={400} gutterWidth={3}>
         {paintings.map((painting) => (
-          <PaintingCard
-            key={painting.id}
-            className="painting-card"
-            id={painting.id}
-            name={painting.name}
-            image={painting.image}
-          />
+          <a role="button" href="#" onClick={toggle}>
+            <PaintingCard
+              key={painting.id}
+              className="painting-card"
+              id={painting.id}
+              name={painting.name}
+              image={painting.image}
+            />
+            <Modal isOpen={modal} toggle={toggle}>
+              <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+              <ModalBody>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </ModalBody>
+              <ModalFooter>
+                <Button color="primary" onClick={toggle}>
+                  Do Something
+                </Button>{" "}
+                <Button color="secondary" onClick={toggle}>
+                  Cancel
+                </Button>
+              </ModalFooter>
+            </Modal>
+          </a>
         ))}
       </StackGrid>
     </div>
