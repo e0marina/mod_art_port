bf2353eb9afb95395937f47bc2cac73878896bd8
Emily M <30880308+e0marina@users.noreply.github.com>
2020-12-30 10:51:08 -0800

    added build folder again

---
+++ client/build/precache-manifest.8aae94e2b63273c467acb6f9eb218d4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe4afd92a4903a587853",
+    "url": "/static/css/main.03e75c5a.chunk.css"
+  },
+  {
+    "revision": "fe4afd92a4903a587853",
+    "url": "/static/js/main.a9af1d12.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ecec5619cfd4e3eb5d1e",
+    "url": "/static/css/2.0e928eb3.chunk.css"
+  },
+  {
+    "revision": "ecec5619cfd4e3eb5d1e",
+    "url": "/static/js/2.8af808ac.chunk.js"
+  },
+  {
+    "revision": "5aacf5ff6ed2401c15d58d42b27d8aa6",
+    "url": "/index.html"
+  }
+];
