dd8a99a64fd6a34bb561e1a9d4829338b5e98421
e0marina <emilymarinaro@gmail.com>
2021-06-19 11:02:12 -0700

    tested and updated art routes

--- controllers/api/art-routes.js
+++ controllers/api/art-routes.js
@@ -2,45 +2,45 @@ const router = require('express').Router();
 const { Art } = require('../../models');
 // The `/api/artwork` endpoint
 router.get('/', (req, res) => {
-    Art.findAll({})
-        .then((artwork) => res.json(artwork))
-        .catch((err) => res.status(500).json(err));
-})
+  Art.findAll({})
+    .then((artwork) => res.json(artwork))
+    .catch((err) => res.status(500).json(err));
+});
 
 router.get('/:id', (req, res) => {
-    Art.findOne({
-        where: {
-            id: req.params.id
-        },
-    })
-        .then((artwork) => res.json(artwork))
-        .catch((err) => res.status(400).json(err))
-})
+  Art.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((artwork) => res.json(artwork))
+    .catch((err) => res.status(400).json(err));
+});
 
 router.post('/', (req, res) => {
-    Art.create(req.body)
-        .then((artwork) => res.status(200).json(artwork))
-        .catch((err) => res.status(400).json(err))
-})
+  Art.create(req.body)
+    .then((artwork) => res.status(200).json(artwork))
+    .catch((err) => res.status(400).json(err));
+});
 
 router.put('/:id', (req, res) => {
-    Art.update(req.body, {
-        where: {
-            id: req.params.id,
-        },
-    })
-        .then((artwork) => res.status(200).json(category))
-        .catch((err) => res.status(400).json(err))
+  Art.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((artwork) => res.status(200).json(artwork))
+    .catch((err) => res.status(400).json(err));
 });
 
 router.delete('/:id', (req, res) => {
-    Art.destroy({
-        where: {
-            id: req.params.id,
-        },
-    })
-        .then((artwork) => res.status(200).json(artwork))
-        .catch((err) => res.status(400).json(err));
+  Art.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((artwork) => res.status(200).json(artwork))
+    .catch((err) => res.status(400).json(err));
 });
 
 module.exports = router;
