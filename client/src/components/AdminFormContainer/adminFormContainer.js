7b78db9a5e63d2fdbcd51ba6f453163d56b7ac0a
e0marina <emilymarinaro@gmail.com>
2021-06-22 20:34:18 -0700

    Mui component for admin login form looks good, needs functionality next

--- client/src/components/AdminFormContainer/adminFormContainer.js
+++ client/src/components/AdminFormContainer/adminFormContainer.js
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   gridItem: {
@@ -11,6 +13,21 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     height: 500,
     width: 500,
+  },
+  form: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '30ch',
+    },
+  },
+  txtfield: {
+    marginLeft: '33%',
+    paddingTop: '150px',
+  },
+  button: {
+    flexDirection: 'column',
+    marginLeft: '33%',
+    width: '20ch',
   },
 }));
 
@@ -21,7 +38,17 @@ const FormContainer = () => {
       <Container>
         <Grid container justify='center'>
           <Grid item className={classes.gridItem}>
-            <Paper elevation={3} className={classes.paper} />
+            <Paper elevation={3} className={classes.paper}>
+              <form className={classes.form} noValidate autoComplete='off'>
+                <div className={classes.txtfield}>
+                  <TextField id='standard-basic' label='EMAIL' />
+                  <TextField id='standard-basic' label='PASSWORD' />
+                </div>
+                <Button className={classes.button} variant='contained'>
+                  Submit
+                </Button>
+              </form>
+            </Paper>
           </Grid>
         </Grid>
       </Container>
