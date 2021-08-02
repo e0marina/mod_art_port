8f8cc19a40c0c083df34c75f3eae2c139c885aae
e0marina <emilymarinaro@gmail.com>
2021-08-02 09:31:02 -0700

    updated routes and working on auth

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -11,21 +11,14 @@ router.post('/login', (req, res) => {
       res.status(400).json({ message: 'No user account found!' });
       return;
     }
-
+    //uses checkPassword method from User model
     const validPassword = dbUserData.checkPassword(req.body.password);
 
     if (!validPassword) {
       res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
-
-    req.session.save(() => {
-      req.session.userId = dbUserData.id;
-      req.session.username = dbUserData.username;
-      req.session.loggedIn = true;
-
-      res.json({ user: dbUserData, message: 'You are now logged in!' });
-    });
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
   });
 });
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const adminRoutes = require('./admin-routes');
+// const adminRoutes = require('./admin-routes');
 
 router.use('/api', apiRoutes);
-router.use('/', adminRoutes);
+// router.use('/', adminRoutes);
 
 router.use((req, res) => {
   res.send('<h1>Wrong Route!</h1>');
